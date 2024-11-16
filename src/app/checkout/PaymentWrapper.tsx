214a2343edb613dccb34bd62504cf5766fb119c2
Ricy <ricyzhang951@gmail.com>
2024-11-16 15:57:58 +0700

    refactor: paymentWrapper

--- src/app/checkout/PaymentWrapper.tsx
+++ src/app/checkout/PaymentWrapper.tsx
@@ -6,23 +6,25 @@ import MultiPayment from "./MultiPayment";
 
 const PaymentWrapper: React.FC = () => {
     const searchParams = useSearchParams();
-    const reciptAddr = searchParams.get("reciptAddr");
+    const reciptAddr = searchParams.get("merchantAddr");
     const referralAddr = searchParams.get("referralAddr");
     const percent = searchParams.get("percent");
     const totalAmount = searchParams.get("totalAmount");
     const productId = searchParams.get("productId");
 
-    if (!reciptAddr || !referralAddr || !percent || !totalAmount || !productId)
+    if (!reciptAddr || !referralAddr || !percent || !totalAmount)
         return <ErrorBoundary />;
-    if (percent === "0") return <Payment productId={productId} />;
-    return (
-        <MultiPayment
-            reciptAddr={reciptAddr}
-            referralAddr={referralAddr}
-            percent={percent}
-            totalAmount={totalAmount}
-        />
-    );
+    if (percent !== "0")
+        return (
+            <MultiPayment
+                reciptAddr={reciptAddr}
+                referralAddr={referralAddr}
+                percent={percent}
+                totalAmount={totalAmount}
+            />
+        );
+    if (!productId) return <ErrorBoundary />;
+    return <Payment productId={productId} />;
 };
 
 export default PaymentWrapper;
