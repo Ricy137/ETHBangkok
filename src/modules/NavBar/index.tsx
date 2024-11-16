3337a9d6110b6a3c9e7228bf409047eaad3965ac
Ricy <ricyzhang951@gmail.com>
2024-11-16 12:14:57 +0700

    feat: navigation bar

--- src/modules/NavBar/index.tsx
+++ src/modules/NavBar/index.tsx
@@ -2,6 +2,20 @@
 import {useState, useEffect} from "react";
 import {usePathname} from "next/navigation";
 import Link from "next/link";
+import {
+    ConnectWallet,
+    Wallet,
+    WalletDropdown,
+    WalletDropdownLink,
+    WalletDropdownDisconnect,
+} from "@coinbase/onchainkit/wallet";
+import {
+    Address,
+    Avatar,
+    Name,
+    Identity,
+    EthBalance,
+} from "@coinbase/onchainkit/identity";
 import cx from "clsx";
 import Mobile from "./Mobile";
 
@@ -93,6 +107,34 @@ const Navbar: React.FC = () => {
                             id="checkout-nav"
                         />
                     </div>
+                </div>
+                <div className="wallet-container">
+                    <Wallet>
+                        <ConnectWallet>
+                            <Avatar className="h-6 w-6" />
+                            <Name />
+                        </ConnectWallet>
+                        <WalletDropdown>
+                            <Identity
+                                className="px-4 pt-3 pb-2"
+                                hasCopyAddressOnClick
+                            >
+                                <Avatar />
+                                <Name />
+                                <Address />
+                                <EthBalance />
+                            </Identity>
+                            <WalletDropdownLink
+                                icon="wallet"
+                                href="https://keys.coinbase.com"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Wallet
+                            </WalletDropdownLink>
+                            <WalletDropdownDisconnect />
+                        </WalletDropdown>
+                    </Wallet>
                 </div>
                 <div className="flex flex-row items-center gap-x-[8px]">
                     <label
